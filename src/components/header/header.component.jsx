f4651d97fcd56f8ef3ed9f6b143df3372e0a6e81
Emily Lin <chestnut321@hotmail.com>
2021-09-12 21:19:20 -0500

    finished implementing redux for user part

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+
 import { Link } from "react-router-dom";
-
 import { auth } from "../../firebase/firebase.utils";
-
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { connect } from "react-redux";
 
 import "./header.styles.scss";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -32,4 +32,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
