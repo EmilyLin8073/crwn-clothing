2c14c21fc6ddc53af47b2c1f5658f77c8593fce3
Emily Lin <chestnut321@hotmail.com>
2021-09-12 20:22:28 -0500

    added store

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
