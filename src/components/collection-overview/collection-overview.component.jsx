222e45900771531e763343612f37b1ce9738ca45
Emily Lin <chestnut321@hotmail.com>
2021-09-19 21:01:10 -0500

    chenaged to use hash table for shop data

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -4,7 +4,7 @@ import { createStructuredSelector } from "reselect";
 
 import "./collection-overview.styles.scss";
 import CollectionPreview from "../collection-preview/collection-preview";
-import { selectCollections } from "../../redux/shop/shop.selectos";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectos";
 
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
@@ -15,7 +15,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-import CollectionsOverview from "../../components/collection-overview/collection-overview.component";
+import { Route } from "react-router-dom";
 
-const ShopPage = () => (
+import CollectionsOverview from "../../components/collection-overview/collection-overview.component";
+import CollectionPage from "../collection/collection.component";
+
+const ShopPage = ({ match }) => (
   <div className="shoe-page">
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
