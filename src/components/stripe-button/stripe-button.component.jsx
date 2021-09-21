68832ccf4f6f4a033d3a17c4212be76ead524fcd
Emily Lin <chestnut321@hotmail.com>
2021-09-20 20:27:47 -0500

    added stripe for paying method

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JbxNTBTx7Hgbr63VRlQUR5b1EMqkwAMCiepEFd9TCx9s5odGCI5x4r27vezPDDnbxi7ZURFl3nYbHc7hXPbhE0O00lAQWf2Eq";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
